6add8060922bc20899d592ca34ad6d3915f15a32
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-10-31 17:44:43 +0300

    event and bubble event

--- src/JS/Js01/script01.js
+++ src/JS/Js01/script01.js
@@ -1,16 +1,20 @@
-stop:for (let i=0;i<5;i++){
-    //debugger
-    for (let j=0;j<3;j++){
-        if(i==2) break stop;
-        console.log(i);
-    }
-}
-const obj=[{name:'bob'},{name: 'ann'},{name:'tom'}];
-const copy=obj.map((x,i)=>({...x,age:i%2==0?20+Math.trunc(Math.random()*10+1):25-Math.trunc(Math.random()*10+1)}));
-console.log(obj);
-console.log(copy);//?
-let arrayRandomValue=new Uint8Array(10);
-arrayRandomValue=crypto.getRandomValues(arrayRandomValue).sort((a,b)=>a-b);
-console.log(arrayRandomValue);
-const copy2=obj.map((x,i)=>({...x,id:arrayRandomValue[i]}));
-console.log(copy2);//?
+const $box1 = document.querySelector('.box1');
+const $box2 = document.querySelector('.box2');
+
+const listener1 = (e) => {
+    console.log(e.currentTarget.className);
+};
+$box1.addEventListener('click', listener1,);
+const listener = (e) => {
+   e.stopPropagation();
+   // console.log(e)
+    console.log( e.currentTarget.className);
+    console.log(e.target.tagName)
+    if(e.target.tagName=='BUTTON')
+        console.log(e.target.name);
+};
+$box2.addEventListener('click', listener,);
+//$box1.removeEventListener('click',listener1);
+document.body.addEventListener('click',(e)=>{
+    console.log(e.currentTarget);
+});
