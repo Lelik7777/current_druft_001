6e4007acf1792d701ea1a86aad3b89901b6f8227
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-11-07 16:48:33 +0300

    add method reduce()

--- src/JS/Js01/script01.js
+++ src/JS/Js01/script01.js
@@ -1,20 +1,27 @@
-const $box1 = document.querySelector('.box1');
-const $box2 = document.querySelector('.box2');
-
-const listener1 = (e) => {
-    console.log(e.currentTarget.className);
-};
-$box1.addEventListener('click', listener1,);
-const listener = (e) => {
-   e.stopPropagation();
-   // console.log(e)
-    console.log( e.currentTarget.className);
-    console.log(e.target.tagName)
-    if(e.target.tagName=='BUTTON')
-        console.log(e.target.name);
-};
-$box2.addEventListener('click', listener,);
-//$box1.removeEventListener('click',listener1);
-document.body.addEventListener('click',(e)=>{
-    console.log(e.currentTarget);
-});
+const map = new Map();
+map.set(1, 'num1');
+console.log(map.get(1));//?
+const lists = [
+    {id: 1, title: 'list1', filter: 'all',value1:34},
+    {id: 1, title: 'list2', filter: 'all',value1: 44},
+    {id: 1, title: 'list3', filter: 'all',value1: 23},
+]
+const tasks = {
+    [lists[0].id]: {},
+    [lists[1].id]: {},
+}
+const arr=[1,3,4,5,7,4];
+const arrR=arr.reduce((a,x)=>a+x,0);
+console.log(arrR);
+console.log(arr.reduce((a,x)=>a+x),0);
+console.log(arr.reduce((a,x)=>a<x?x:a));//?
+console.log(lists.reduce((acc,el)=>acc +el. value1,0));//?
+console.log(lists.reduce((a, x)=>{x.value1>30&&(a[x.title]=x.value1); return a},{}));//?
+const reduceMy = (arr) => {
+  let a=[];
+  for (let k of arr){
+      a.push(k+'element');
+  }
+    return a;
+}
+console.log(reduceMy(arr));//?
